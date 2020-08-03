e00ea7352c39b9442a1043830f433bdb2fd0255e
huntj0n <hunt.jon19@gmail.com>
2020-08-03 13:50:38 -0600

    css issues

--- src/Components/AddSong.js
+++ src/Components/AddSong.js
@@ -22,20 +22,34 @@ export default class AddSong extends Component {
         this.setState({name: '', artist: ''});
     }
 
+    toggleForm = () => {
+        this.setState({showForm: !this.state.showForm})
+    }
+
     render(){
         return(
             <div className="form">
-                <input 
-                value={this.state.name} 
-                onChange={(e)=> this.nameChange(e.target.value)}
-                placeholder="Enter Song Name"
-                />
-                <input
-                value={this.state.artist}
-                onChange={(e)=> this.artistChange(e.target.value)}
-                placeholder="Enter Artist Name"
-                />
-                <button className="btn-add" onClick={()=>this.save(this.state.name, this.state.artist)}>Add Song</button>
+                {this.state.showForm ? (
+                    <form className="newSongForm">
+                        <input 
+                        className="input-input"
+                        value={this.state.name} 
+                        onChange={(e)=> this.nameChange(e.target.value)}
+                        placeholder="Enter Song Name"
+                        />
+                        <input
+                        className="input-input"
+                        value={this.state.artist}
+                        onChange={(e)=> this.artistChange(e.target.value)}
+                        placeholder="Enter Artist Name"
+                        />
+                        <button className="btn btn-cancel btn-form" onClick={this.toggleForm}>Cancel</button>
+                        <button className="btn-add" onClick={()=>this.save(this.state.name, this.state.artist)}>Add Song</button>
+                    </form> 
+                    ):(
+                        <button className="btn btn-add" onClick={this.toggleForm}>Add A New Song</button>
+                    )    
+                }
             </div>
         )
     }
